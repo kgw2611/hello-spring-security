21a31edb2f9f1e16ecc062118826ed808c0d6b4a
2171147 <kenyouzzang@gmail.com>
2026-06-06 22:51:55 +0900

    feat: Pagination, ProductSearch updated
    상품 5개씩 페이지네이션 기능
    상품 검색 기능

--- src/main/java/kr/ac/hansung/service/ProductService.java
+++ src/main/java/kr/ac/hansung/service/ProductService.java
@@ -10,6 +10,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
@@ -38,5 +41,13 @@ public class ProductService {
     @Transactional
     public void deleteById(Long id) {
         productRepository.deleteById(id);
+    }
+
+    public Page<Product> getProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+
+    public Page<Product> searchProducts(String keyword, Pageable pageable) {
+        return productRepository.findByNameContaining(keyword, pageable);
     }
 }
